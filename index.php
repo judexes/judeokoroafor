2e937aee59b53eb6035c0d6897f28130f0da55d0
jude okoroafor <judeokoroafor@gmail.com>
2020-07-27 13:42:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -471,7 +471,7 @@ Am also strong conceptive thinker who has strong interest in all things relating
                         <li><a href="https://www.facebook.com/JudeOkoroaforIfeanyi"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
                         <li><a href="https://www.instagram.com/dblacklagosboy"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
                         <li><a href="https://www.twitter.com/dblacklagosboy"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
-                        <li><a href=""><span class="ico-circle"><i class="ion-social-pinterest"></i></span></a></li>
+                        <li><a href="#"><span class="ico-circle"><i class="ion-social-pinterest"></i></span></a></li>
                       </ul>
                     </div>
                   </div>
